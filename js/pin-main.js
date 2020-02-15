d2c25b0a62c388c09d7da08c9a3bb540923e46b1
Pavel Nyukalo <PashaOktan07@gmail.com>
2020-02-15 17:36:09 +0500

    Добавляет загрузку данных с сервера

--- js/pin-main.js
+++ js/pin-main.js
@@ -4,12 +4,13 @@
   var PIN_WIDTH = 65;
   var PIN_HEIGHT = 65;
   var PIN_TAIL = 22;
-  var MIN_Y = 130 - PIN_HEIGHT / 2;
-  var MAX_Y = 630 - PIN_HEIGHT / 2;
+  var MIN_Y = 75 - PIN_HEIGHT / 2;
+  var MAX_Y = 575 - PIN_HEIGHT / 2;
   var MIN_X = 0 - PIN_WIDTH / 2;
-  var MAX_X = window.data.widthMap - PIN_WIDTH / 2;
+  var MAX_X = widthMap - PIN_WIDTH / 2;
 
   var map = document.querySelector('.map');
+  var widthMap = map.offsetWidth;
   var mapPinMain = document.querySelector('.map__pin--main');
   var mapPins = document.querySelector('.map__pins');
 
